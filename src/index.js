f08a815019f45ff2e9993bac6b02cec4132742fe
IraMira333 <irinat333@gmail.com>
2023-05-11 23:11:59 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import './css/styles.css';
+import { fetchCountries } from './fetchCountries.js';
 
 const DEBOUNCE_DELAY = 300;
+const inputSearchBoxEl = document.querySelector('#search-box');
+
+inputSearchBoxEl.addEventListener('input', onFetchCountries);
+
+function onFetchCountries(evt) {
+  console.log(evt.target.value);
+}
+console.log(fetchCountries);
+fetchCountries('Ukraine').then(result => console.log(result));
