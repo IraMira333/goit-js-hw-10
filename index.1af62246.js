68b365bbabc64f3491e26e1ac9b862e5af7fe186
IraMira333 <119503034+IraMira333@users.noreply.github.com>
2023-05-12 14:16:58 +0000

    Deploying to gh-pages from @ IraMira333/goit-js-hw-10@9938684005b54c62b5bcc76fc84b36a4ce82c190 🚀

--- index.1af62246.js
+++
@@ -1,2 +0,0 @@
-function e(e){return fetch(`https://restcountries.com/v3.1/name/${e}?fields=name,capital,population,flags,languages\n  `).then((e=>e.json()))}document.querySelector("#search-box").addEventListener("input",(function(e){console.log(e.target.value)})),console.log(e),e("Ukraine").then((e=>console.log(e)));
-//# sourceMappingURL=index.1af62246.js.map
