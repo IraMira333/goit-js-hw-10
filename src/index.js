9938684005b54c62b5bcc76fc84b36a4ce82c190
IraMira333 <irinat333@gmail.com>
2023-05-12 17:16:35 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './css/styles.css';
 import { fetchCountries } from './fetchCountries.js';
+import { Notify } from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
 const inputSearchBoxEl = document.querySelector('#search-box');
@@ -8,6 +9,40 @@ inputSearchBoxEl.addEventListener('input', onFetchCountries);
 
 function onFetchCountries(evt) {
   console.log(evt.target.value);
+  let nameCountry = evt.target.value.trim();
+  if (nameCountry === '') {
+    return;
+  }
+  fetchCountries(nameCountry)
+    .then(result => {
+      console.log(result.length);
+      if (result.length > 10) {
+        return tooManyCountries(result);
+      }
+      console.log(markupList(result));
+    })
+    //.then(arrCountries => console.log(markupList(arrCountries)))
+    .catch(onError);
 }
 console.log(fetchCountries);
-fetchCountries('Ukraine').then(result => console.log(result));
+
+function tooManyCountries(result) {
+  return Notify.info(
+    'Too many matches found. Please enter a more specific name.'
+  );
+}
+
+function onError(err) {
+  Notify.failure('Oops, there is no country with that name');
+}
+
+function markupList(arrCountries) {
+  return arrCountries
+    .map(
+      ({ name: { official }, flags: { svg } }) => `<li>
+    <img src='${svg}' alt='Flag of ${official}'>
+    <h2>${official}</h2>
+  </li>`
+    )
+    .join('');
+}
